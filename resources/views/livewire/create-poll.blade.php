76b3050787be02b85d370a43b37ae8deec499197
Douglas White <douglas.rw@outlook.com>
2024-01-05 16:50:56 -0700

    update

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,9 +1,30 @@
 <div>
     <form>
+
       <label>Poll Title</label>
   
       <input type="text" wire:model.live="title" />
   
       Current title: {{ $title }}
+
+      <div  class="mb-4 mt-4">
+        <button class='btn' wire:click.prevent="addOption">Add Option</button>
+      </div>
+
+    <div>
+      @foreach ($options as $index => $option)
+        <div class="mb-4">
+          <label>Option {{ $index + 1 }}</label>
+          {{ $index }} - {{ $option }}
+          <div class="flex gap-2">
+            <input type='text' wire:model="options.{{ $index }}"/>
+            <button class="btn" wire:click.prevent="removeOption({{$index}})">Remove</button>
+          </div>
+        </div>
+
+
+      @endforeach
+    </div>
+
     </form>
   </div>
